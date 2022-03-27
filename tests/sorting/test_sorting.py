1f55a192cd49a9704d48f7185465fe853524ab3b
MarcusVMendes <marcus.mendes89@gmail.com>
2022-03-27 00:43:28 -0300

    Requisito 10 concluido

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-03-27"},
+        {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-27"},
+        {"min_salary": 9000, "max_salary": 11000, "date_posted": "2022-03-27"},
+    ]
+
+    result_min_salary = [jobs[0], jobs[1], jobs[2]]
+    result_max_salary = [jobs[2], jobs[1], jobs[0]]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == result_min_salary
+
+    sort_by(jobs, "max_salary")
+    assert jobs == result_max_salary
